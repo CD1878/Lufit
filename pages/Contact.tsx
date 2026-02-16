837592f922b27810ecbd85fb96e087e4a3323e2e
Johan Tijsseling <jtijs@MacBook-Pro-van-Johan.local>
2026-02-16 17:17:12 +0100

    Improve mobile layout overlap on Contact page

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -11,7 +11,7 @@ const Contact: React.FC<Props> = ({ onNavigate }) => {
     return (
         <div className="fade-in bg-gray-50 flex flex-col min-h-screen">
             {/* Header Section */}
-            <div className="bg-anthracite text-white pt-12 pb-32 md:pt-24 md:pb-48 rounded-b-[3rem] relative shadow-xl overflow-hidden mb-[-50px] md:mb-[-100px]">
+            <div className="bg-anthracite text-white pt-12 pb-32 md:pt-24 md:pb-48 rounded-b-[3rem] relative shadow-xl overflow-hidden mb-[-80px] md:mb-[-100px]">
                 <div className="absolute top-0 left-0 w-full overflow-hidden leading-none opacity-10 rotate-180">
                     <svg className="relative block w-[calc(110%+1.3px)] h-[80px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" fill="currentColor"></path>
@@ -25,7 +25,7 @@ const Contact: React.FC<Props> = ({ onNavigate }) => {
                 </div>
             </div>
 
-            <div className="container mx-auto px-4 py-12 md:py-16 relative z-10 flex-grow">
+            <div className="container mx-auto px-4 pb-12 md:py-16 relative z-10 flex-grow">
                 <div className="bg-white rounded-[3rem] shadow-2xl overflow-hidden max-w-6xl mx-auto">
                     <div className="grid lg:grid-cols-2">
                         {/* Info Side */}
